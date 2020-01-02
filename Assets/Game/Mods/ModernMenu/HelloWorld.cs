51c9b599750a83a26d467bc7ad2f2272e2b0e502
numidium <rbrt.caine@gmail.com>
2020-01-01 19:22:28 -0500

    begin testing skeleton ui

--- Assets/Game/Mods/ModernMenu/HelloWorld.cs
+++ Assets/Game/Mods/ModernMenu/HelloWorld.cs
@@ -1,47 +1,65 @@
-﻿using UnityEngine;
-using System.Collections;
+using UnityEngine;
 using DaggerfallWorkshop.Game;
-using DaggerfallWorkshop.Game.UserInterface;
-using DaggerfallWorkshop.Game.UserInterfaceWindows; //required for pop-up window
-using DaggerfallWorkshop.Game.Utility.ModSupport;   //required for modding features
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+using DaggerfallWorkshop.Utility;
+using DaggerfallConnect.Utility;
 
-namespace DaggerfallModdingTutorials.Part2
+namespace ModernMenu
 {
     public class HelloWorld : MonoBehaviour
     {
-        static DaggerfallMessageBox helloWorldMessage;
-        public bool check = false;
+        Rect characterSheetWindow = new Rect(5, 5, 300, 400);
+        Rect inventoryWindow = new Rect(5, 310, 400, 300);
+        Rect magicWindow = new Rect(500, 5, 200, 300);
+        bool isVisible = false;
+        const string baseTextureName = "INVE00I0.IMG";
 
-        IEnumerator Start()
+        void Start()
         {
-            //delay for a few seconds then display the Hello World message
-            yield return new WaitForSeconds(2);
-            //push the message box
-            DisplayMessage();
+            Debug.Log("Modern menu Start() called.");
         }
 
+        void OnGUI()
+        {
+            if (isVisible)
+            {
+                int windowId = 0;
+                characterSheetWindow = GUI.Window(windowId++, characterSheetWindow, DoTestWindow, "Character");
+                GUI.depth = 1;
+                inventoryWindow = GUI.Window(windowId++, inventoryWindow, DoTestWindow, "Inventory");
+                GUI.depth = -1;
+                Texture2D baseTexture = ImageReader.GetTexture(baseTextureName);
+                Rect wagonButtonRect = new Rect(226, 14, 31, 14);
+                DFSize baseSize = new DFSize(320, 200);
+                Texture2D buttonTexture = ImageReader.GetSubTexture(baseTexture, wagonButtonRect, baseSize);
+                GUI.Button(new Rect(inventoryWindow.x + 10, inventoryWindow.y + 20, 100f, 100f), new GUIContent(buttonTexture, "tooltip"));
+                magicWindow = GUI.Window(windowId, magicWindow, DoTestWindow, "Magic");
+                GUI.Label(new Rect(Input.mousePosition.x, Input.mousePosition.y, 100, 40), GUI.tooltip);
+            }
+        }
+
+        void DoTestWindow(int windowId)
+        {
+            GUI.DragWindow(new Rect(0, 0, 10000, 40));
+        }
 
         void Update()
         {
-            //display another message when the player dies
-            //open the console (~) and type suicide to test
-            if (GameManager.Instance.StateManager.CurrentState == StateManager.StateTypes.Death)
+            if (Input.GetMouseButtonDown(1))
             {
-                if (check)
+                if (!isVisible)
                 {
-                    check = false;
-                    DisplayMessage("hahahaha, you died.");
+                    GameManager.Instance.PauseGame(true);
+                    isVisible = true;
+                }
+                else
+                {
+                    GameManager.Instance.PauseGame(false);
+                    isVisible = false;
                 }
             }
-            else if (check == false)
-                check = true;
         }
 
-        //this method will be called automatically by the modmanager after the main game scene is loaded.
-        //The following requirements must be met to be invoked automatically by the ModManager during setup for this to happen:
-        //1. Marked with the [Invoke] custom attribute
-        //2. Be public & static class method
-        //3. Take in an InitParams struct as the only parameter
         [Invoke(StateManager.StateTypes.Game, 0)]
         public static void Init(InitParams initParams)
         {
@@ -54,42 +72,5 @@ namespace DaggerfallModdingTutorials.Part2
             //after finishing, set the mod's IsReady flag to true.
             ModManager.Instance.GetMod(initParams.ModTitle).IsReady = true;
         }
-
-        //You can have as many of these methods as you want to call by the ModManager
-        //If you pass an integer to the Invoke constructor, you can control the order in which they load
-        //lower loads sooner.  Note that this is per MOD not per-class.
-        [Invoke(StateManager.StateTypes.Game, 1)]
-        public static void Init2(InitParams initParams)
-        {
-            Debug.Log("init 2");
-
-        }
-
-        //the 2nd paramater allows you to control which state the ModManager will invoke this method - the Start state
-        //-that's when the scene is first loaded, or the game state - when a game is started (either a new game, or a save is loaded)
-        [Invoke(StateManager.StateTypes.Start)]
-        public static void InitStart(InitParams initParams)
-        {
-            Debug.Log("init at Start");
-        }
-
-        /// <summary>
-        /// pushes the hello world message box onto the stack
-        /// </summary>
-        public static void DisplayMessage(string message = "Hello World!")
-        {
-            if (helloWorldMessage == null)
-            {
-                helloWorldMessage = new DaggerfallMessageBox(DaggerfallWorkshop.Game.DaggerfallUI.UIManager);
-                helloWorldMessage.AllowCancel = true;
-                helloWorldMessage.ClickAnywhereToClose = true;
-                helloWorldMessage.ParentPanel.BackgroundColor = Color.clear;
-            }
-
-            helloWorldMessage.SetText(message);
-            DaggerfallUI.UIManager.PushWindow(helloWorldMessage);
-        }
-
-
     }
 }
