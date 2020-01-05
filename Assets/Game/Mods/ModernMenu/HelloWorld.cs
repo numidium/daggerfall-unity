fac299c395c9a3976578c7ab460497eac9cfd7fe
numidium <rbrt.caine@gmail.com>
2020-01-05 02:33:25 -0500

    Add UI prefab

--- Assets/Game/Mods/ModernMenu/HelloWorld.cs
+++ Assets/Game/Mods/ModernMenu/HelloWorld.cs
@@ -8,39 +8,11 @@ namespace ModernMenu
 {
     public class HelloWorld : MonoBehaviour
     {
-        Rect characterSheetWindow = new Rect(5, 5, 300, 400);
-        Rect inventoryWindow = new Rect(5, 310, 400, 300);
-        Rect magicWindow = new Rect(500, 5, 200, 300);
         bool isVisible = false;
-        const string baseTextureName = "INVE00I0.IMG";
 
         void Start()
         {
             Debug.Log("Modern menu Start() called.");
-        }
-
-        void OnGUI()
-        {
-            if (isVisible)
-            {
-                int windowId = 0;
-                characterSheetWindow = GUI.Window(windowId++, characterSheetWindow, DoTestWindow, "Character");
-                GUI.depth = 1;
-                inventoryWindow = GUI.Window(windowId++, inventoryWindow, DoTestWindow, "Inventory");
-                GUI.depth = -1;
-                Texture2D baseTexture = ImageReader.GetTexture(baseTextureName);
-                Rect wagonButtonRect = new Rect(226, 14, 31, 14);
-                DFSize baseSize = new DFSize(320, 200);
-                Texture2D buttonTexture = ImageReader.GetSubTexture(baseTexture, wagonButtonRect, baseSize);
-                GUI.Button(new Rect(inventoryWindow.x + 10, inventoryWindow.y + 20, 100f, 100f), new GUIContent(buttonTexture, "tooltip"));
-                magicWindow = GUI.Window(windowId, magicWindow, DoTestWindow, "Magic");
-                GUI.Label(new Rect(Input.mousePosition.x, Input.mousePosition.y, 100, 40), GUI.tooltip);
-            }
-        }
-
-        void DoTestWindow(int windowId)
-        {
-            GUI.DragWindow(new Rect(0, 0, 10000, 40));
         }
 
         void Update()
